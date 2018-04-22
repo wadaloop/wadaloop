58edc1991066681da8bf08aed5c80661a92dcef5
Alejandro <alexibape@hotmail.com>
2018-04-22 21:43:10 +0200

    First Commit, user Autentication OK, Session OK, Login with email OK.

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
@@ -69,8 +70,12 @@ app.use(session({
   secret: 'irongenerator',
   resave: true,
   saveUninitialized: true,
-  store: new MongoStore( { mongooseConnection: mongoose.connection })
-}))
+  store: new MongoStore( { 
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day 
+  })
+}));
+
 app.use(flash());
 require('./passport')(app);
     
