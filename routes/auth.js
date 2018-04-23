e1a66d112687117c3cc4efd7e7aeebfd541c03e2
Pablo García <heypablete@gmail.com>
2018-04-23 15:17:03 +0200

    algo

--- routes/auth.js
+++ routes/auth.js
@@ -57,6 +57,36 @@ authRoutes.post("/signup", (req, res, next) => {
   });
 });
 
+authRoutes.get("/profile", (req, res, next) => {
+  res.render("auth/profile");
+});
+
+authRoutes.post("/profile", (req, res, next) => {
+  console.log(req.body);
+
+  const productTitle = req.body.productTitle;
+  const productDescription = req.body.productDescription;
+//  const idUser = req.body._id;
+
+    const newProduct = new Product({
+      title: productTitle,
+      description: productDescription,
+      price: productPrice,
+      photo: productPhoto,
+      // ENTRAR DENTRO DEL USUARIO Y DECIR QUE SELLER: TRUE
+      // idUser: idUser
+    });
+
+    newProduct.save((err) => {
+      if (err) {
+        res.render("auth/profile", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+
+});
+
 authRoutes.get("/logout", (req, res) => {
   req.logout();
   res.redirect("/");
