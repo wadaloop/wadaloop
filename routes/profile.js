fb3535a8e73f5cd0d681e0d12dd685caaff34ba8
Pablo García <heypablete@gmail.com>
2018-04-23 18:47:38 +0200

    imagenes subidas por CDN

--- routes/profile.js
+++ routes/profile.js
@@ -1,47 +1,45 @@
 const express = require("express");
-const passport = require('passport');
+const passport = require("passport");
 const profileRoutes = express.Router();
+const multer = require("multer");
 const User = require("../models/User");
 const Product = require("../models/Product");
+const uploadCloud = require("../middlewares/cloudinary.js");
 const ensureLoggedIn = require("../middlewares/ensureLoggedIn");
-
 
 profileRoutes.get("/", ensureLoggedIn("/login"), (req, res, next) => {
   res.render("profile");
 });
 
-profileRoutes.post("/", (req, res, next) => {
-  console.log("----------req.body---------")
-  console.log(req.body);
-  console.log("xx")
-  console.log("----------req.session---------")
-  console.log(req.session);
-  console.log("xx")
-  console.log("----------req.session.passport.user---------")
-  console.log(req.session.passport.user);
-  console.log("xx")
-  const productTitle = req.body.productTitle
-  const productDescription = req.body.productDescription
-  const productPrice = req.body.productPrice
-  const newProduct = new Product({
-    title: productTitle,
-    description: productDescription,
-    price: productPrice,
-    user: req.session.passport.user
-    //photo: productPhoto,
-    // ENTRAR DENTRO DEL USUARIO Y DECIR QUE SELLER: TRUE
-  });
+profileRoutes.post(
+  "/",
+  uploadCloud.single("productPhoto"),
+  (req, res, next) => {
+    const productTitle = req.body.productTitle;
+    const productDescription = req.body.productDescription;
+    const productPrice = req.body.productPrice;
 
-  newProduct.save((err) => {
-    if (err) {
-      console.log("error")
-      res.render("profile", { message: "Something went wrong" });
-    } else {
-      res.redirect("/");
-    }
-  });
+    const imgName = req.file.originalname;
+    const imgPath = req.file.url;
 
-});
+    const newProduct = new Product({
+      title: productTitle,
+      description: productDescription,
+      price: productPrice,
+      user: req.session.passport.user,
+      imgName: imgName,
+      imgPath: imgPath
+    });
 
+    newProduct.save(err => {
+      if (err) {
+        console.log("error");
+        res.render("profile", { message: "Something went wrong" });
+      } else {
+        res.redirect("/");
+      }
+    });
+  }
+);
 
 module.exports = profileRoutes;
