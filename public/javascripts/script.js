48d91efae969d7d1ff06e31453d9cbe9ba25d082
Pablo García <heypablete@gmail.com>
2018-04-25 16:01:53 +0200

    mapa listo

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,25 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-var lat = parseFloat(document.getElementById("lat").innerHTML);
-var long = parseFloat(document.getElementById("long").innerHTML);
-console.log(lat)
-console.log(long)
-
-  function startMap() {
-    const ironhackBCN = {
-      lat: lat,
-      lng: long,
-      travelMode: 'DRIVING'
-    };
-    const map = new google.maps.Map(
-      document.getElementById('map'),
-      {
-        zoom: 15,
-        center: ironhackBCN
-      }
-    );
-  }
+  var lat = parseFloat(document.getElementById("lat").innerHTML);
+  var lon = parseFloat(document.getElementById("lon").innerHTML);
   
-  startMap();
-
-}, false);
+    function startMap() {
+      const productPosition = { lat: lat, lng: lon };
+      const map = new google.maps.Map(
+        document.getElementById('map'),
+        {
+          zoom: 15,
+          center: productPosition
+        }
+      );
+      var marker = new google.maps.Marker({
+        position: productPosition,
+        map: map
+      });
+    }
+    
+    startMap();
+  
+  }, false);
